5c89b3f90ed41782ea2b7105dd19eeda0ecb84c4
Felipe Monteiro <felipe.cife@gmail.com>
2019-04-07 12:49:27 -0300

    deploy jenkins in tomcat container

--- src/main/java/br/com/heroku/Boot.java
+++ src/main/java/br/com/heroku/Boot.java
@@ -2,13 +2,12 @@ package br.com.heroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Boot
-{
+public class Boot extends SpringBootServletInitializer{
    //testing jenkins deploy
-   public static void main(String[] args)
-   {
+   public static void main(String[] args){
       SpringApplication.run(Boot.class, args);
    }
 }
